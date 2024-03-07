f053dad37ffe62c910c0b0234ecaf8ef575b503b
Josiel Junior <juniorfoxjbs@gmail.com>
2024-03-07 11:01:45 -0300

    Refatoração da validação do statusCode e criação de uma abstração para os métodos given(), when() e then()

--- src/test/java/features/tests/lojinha/usuario/AdicionarUsuarioTest.java
+++ src/test/java/features/tests/lojinha/usuario/AdicionarUsuarioTest.java
@@ -4,7 +4,9 @@ import com.github.javafaker.Faker;
 import features.clients.lojinha.BaseConfig;
 import features.clients.lojinha.usuario.BaseUsuarioPath;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.data.lojinha.data_factory.UsuarioDataFactory;
@@ -14,39 +16,36 @@ import static org.hamcrest.Matchers.*;
 
 public class AdicionarUsuarioTest {
     Faker faker = new Faker();
-    @BeforeEach
-    public void setUp(){
+    private static final String NOME_USUARIO = "Jhon";
+    // O ideal seria exluir o usuário ao final do teste e cadastrar novamente. Mas a API não tem o endpoint de exclusão.
+    @BeforeAll
+    public static void setUp(){
         BaseConfig.configApis();
     }
-
     @Test
     @DisplayName("Validar cadastro de usuário")
     public void validarCadastroUsuario(){
-        String nome = faker.name().firstName();
-        String login = faker.name().firstName() + "@";
-        given()
-            .contentType(ContentType.JSON)
-            .body(UsuarioDataFactory.criarNovoUsuario(nome, login, "123"))
-        .when()
-            .post(BaseUsuarioPath.getPath())
-        .then()
-            .assertThat()
-                .statusCode(201)
-                .body("data.usuarioNome", equalTo(nome))
-                .body("data.usuarioLogin", equalTo(login))
-                .body("message", equalTo("Usuário adicionado com sucesso"));
+        String usuarioLogin = faker.name().firstName() + "@";
+        cadastrarUsuario(usuarioLogin).assertThat()
+            .statusCode(HttpStatus.SC_CREATED)
+            .body("data.usuarioNome", equalTo(NOME_USUARIO))
+            .body("data.usuarioLogin", equalTo(usuarioLogin))
+            .body("message", equalTo("Usuário adicionado com sucesso"));
     }
     @Test
-    @DisplayName("Validar tentativa de cadastro com usuário duplicado")
+    @DisplayName("Validar tentativa de cadastro para usuário já cadastrado")
     public void validarTentativaCadastroUsuarioDuplicado(){
-        given()
+        String usuarioLogin = "jhon@gmail";
+        cadastrarUsuario(usuarioLogin).assertThat()
+            .statusCode(HttpStatus.SC_CONFLICT)
+            .body("error", equalTo("O usuário " + usuarioLogin + " já existe."));
+    }
+    private ValidatableResponse cadastrarUsuario(String usuarioLogin){
+        return   given()
             .contentType(ContentType.JSON)
-            .body(UsuarioDataFactory.criarNovoUsuario("jhon", "jhon", "123"))
+            .body(UsuarioDataFactory.criarNovoUsuario(NOME_USUARIO, usuarioLogin, "123"))
         .when()
             .post(BaseUsuarioPath.getPath())
-        .then()
-            .assertThat()
-                .statusCode(409)
-                .body("error", equalTo("O usuário jhon já existe."));
+        .then();
     }
 }
--- src/test/java/features/tests/lojinha/usuario/GerarTokenTest.java
+++ src/test/java/features/tests/lojinha/usuario/GerarTokenTest.java
@@ -3,6 +3,8 @@ package features.tests.lojinha.usuario;
 import features.clients.lojinha.BaseConfig;
 import features.clients.lojinha.usuario.BaseLoginPath;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,32 +14,29 @@ import static org.hamcrest.Matchers.*;
 
 public class GerarTokenTest {
 
-    @BeforeEach
-    public void setUp(){
+    @BeforeAll
+    public static void setUp(){
         BaseConfig.configApis();
     }
     @Test
     @DisplayName("Validar tentativa de gerar token com usuario invalido")
     public void validarTentativaGerarTokenUsuarioInvalido(){
-        given()
-            .contentType(ContentType.JSON)
-            .body(UsuarioDataFactory.criarUsuarioAutenticacao("usuario_invalido", "senha_invalida"))
-        .when()
-            .post(BaseLoginPath.getPath())
-        .then()
+        gerartoken("usuario_invalido", "senha_invalida").assertThat()
             .statusCode(401);
     }
     @Test
     @DisplayName("Validar geração de token")
     public void validarGeracaoToken(){
-        given()
+        gerartoken("jhon", "123").assertThat()
+            .statusCode(200)
+            .body("message", equalTo("Sucesso ao realizar o login"));
+    }
+    private ValidatableResponse gerartoken(String login, String senha){
+        return   given()
             .contentType(ContentType.JSON)
-            .body(UsuarioDataFactory.criarUsuarioAutenticacao("jhon", "123"))
+            .body(UsuarioDataFactory.criarUsuarioAutenticacao(login, senha))
         .when()
             .post(BaseLoginPath.getPath())
-        .then()
-            .assertThat()
-                .statusCode(200)
-                .body("message", equalTo("Sucesso ao realizar o login"));
+        .then();
     }
 }
